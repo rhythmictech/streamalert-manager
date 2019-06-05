6234c1abe7c0b6d6720402b2f468a14db8456b55
Harish Karumuri <hkarumuri@hq-ltm-1260.corp.rhythmic.net>
2019-06-05 11:05:50 -0400

    Play made to convert templates to rules and clone streamalert repo

---
+++ rules.folder/ duo_bypass_code_create_non_auto_generated.py
@@ -0,0 +1,17 @@
+"""Alert when a DUO bypass code is artisanly crafted and not auto-generated."""
+from rules.helpers.base import safe_json_loads
+from stream_alert.shared.rule import rule
+
+
+@rule(
+    logs=['duo:administrator'], 
+    outputs=['streamalert-testing1'] 
+)
+def duo_bypass_code_create_non_auto_generated(rec):
+    """
+    author:       @mimeframe
+    description:  Alert when a DUO bypass code is artisanly crafted and not auto-generated.
+    reference:    https://duo.com/docs/administration-users#generating-a-bypass-code
+    """
+    return (rec['action'] == 'bypass_create'
+            and safe_json_loads(rec['description']).get('auto_generated') is False)
